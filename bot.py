ecce4af43d867f98556f10f3afcb2986c967699d
Nikita <evrsml@gmail.com>
2023-11-24 09:50:04 +0500

    обход длинных сообщений

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ from creds import tg_token
 bot  = Bot(token=tg_token)
 
 async def send_message(msg):
-    await bot.send_message(chat_id='@mytesr27', text= str(msg))
+        await bot.send_message(chat_id='@mytesr27', text= str(msg))
+    
     
     
    
--- parser_logic.py
+++ parser_logic.py
@@ -5,15 +5,14 @@ import requests
 import time
 from datetime import datetime
 from urllib.parse import urlparse
-from creds import vk_token, tg_token
+from creds import vk_token
 from bot import send_message
-#from aiogram import Bot, Dispatcher
+from redisConfig import r
 
-url = 'https://vk.com/inorz'
-
-
+url = 'https://vk.com/id535331631'
 
 '''Парсим ссылку'''
+
 def user_link_parse(link):
     parsed_url = urlparse(link)
     parts_url = parsed_url.path.split('/')
@@ -29,7 +28,7 @@ def user_link_parse(link):
         id_check = str(id_check)
         last_part.startswith(id_check)
         result = last_part.replace('id', '')
-        get_posts_id(result)
+        get_posts_id(result,get_page_name_by_id(result))
         return result
     
     if public_check is not None:
@@ -37,39 +36,79 @@ def user_link_parse(link):
         last_part.startswith(public_check)
         res = last_part.replace('public', '')
         result = f'-{res}'
-        get_posts_id(result)
+        get_posts_id(result, get_page_name_by_id(result))
         return result
     else:
-        get_posts_name(last_part)
+        get_posts_name(last_part, get_page_name_by_name(last_part))
         return last_part
 
 '''Получаем словарь постов с аккаунта или группы по id'''
-def get_posts_id(id):
-    url = f"https://api.vk.com/method/wall.get?owner_id={id}&count={3}&extended=1&v=5.131&access_token={vk_token}"
+
+def get_posts_id(id, name):
+    url = f"https://api.vk.com/method/wall.get?owner_id={id}&count={2}&extended=1&v=5.199&access_token={vk_token}"
     response = requests.get(url)
     data = json.loads(response.text)
     if 'error' not in data:
-        from_posts_get_post(data)
+        from_posts_get_post(data, name)
         return data
     else:
         print(data['error']['error_msg'])
         pass
 
 '''Получаем словарь постов с аккаунта или группы по короткому имени'''
-def get_posts_name(name):
-    url = f"https://api.vk.com/method/wall.get?domain={name}&count={3}&extended=1&v=5.131&access_token={vk_token}"
+
+def get_posts_name(shortname,name):
+    url = f"https://api.vk.com/method/wall.get?domain={shortname}&count={2}&extended=1&v=5.199&access_token={vk_token}"
     response = requests.get(url)
     data = json.loads(response.text)
     if 'error' not in data:
-        from_posts_get_post(data)
+        from_posts_get_post(data, name)
         return data
     else:
         print(data['error']['error_msg'])
         pass
 
-'''Из вложенного словаря достаем отдельные посты и офрмляем для пересылки'''
+'''Получаем название паблика или аккаунта по айди'''
 
-def from_posts_get_post(data):
+def get_page_name_by_id(id):
+
+    if  str(id).startswith('-'):
+        id = id.split('-')
+        print(id)
+        url_group = f"https://api.vk.com/method/groups.getById?group_id={id[1]}&v=5.199&access_token={vk_token}"
+        response = requests.get(url_group)
+        data = json.loads(response.text)
+        name = data['response']['groups'][0]['name']
+        return name
+    
+    if str(id).isdigit():
+        url_account = f"https://api.vk.com/method/users.get?user_ids={id}&v=5.199&access_token={vk_token}"
+        response = requests.get(url_account)
+        data = json.loads(response.text)
+        last_name = data['response'][0]['last_name']
+        first_name = data['response'][0]['first_name']
+        full_name = f'{first_name} {last_name}'
+        return full_name
+
+'''Получаем название паблика или аккаунта по короткому адресу'''   
+            
+def get_page_name_by_name(name):
+    resolve_name = f"https://api.vk.com/method/utils.resolveScreenName?screen_name={name}&v=5.199&access_token={vk_token}"
+    response = requests.get(resolve_name)
+    data = json.loads(response.text)
+    if data['response']['type'] == 'user':
+        res = data['response']['object_id']
+        result = get_page_name_by_id(res)
+        return result
+    else:
+        data = data['response']['object_id']
+        res = f'-{data}'
+        result = get_page_name_by_id(res)
+        return result
+       
+'''Из вложенного словаря достаем отдельные посты и офрмляем для пересылки'''
+  
+def from_posts_get_post(data, name):
     num = (len(data['response']['items']))
     items = data['response']['items']
 
@@ -80,12 +119,17 @@ def from_posts_get_post(data):
         date = datetime.fromtimestamp(items[i]['date'])
         date_from_post = date.strftime('%Y-%m-%d')
         current_day = date.today().strftime('%Y-%m-%d')
-        if date_from_post == current_day:                                   
+        if date_from_post == current_day:
+
             owner_id = items[i]['owner_id']
             id_post = items[i]['id']
-            result = f"{text}\n\nДата публикации: {date}\nСсылка: https://vk.com/wall{owner_id}_{id_post}"
+            if len(text) > 3950:
+                text_short = text[:3950]
+                result = f"Источник: {name}\n\n{text_short}\n..полный текст по ссылке..\n\nДата публикации: {date}\nСсылка: https://vk.com/wall{owner_id}_{id_post}"
+            else:
+                result = f"Источник: {name}\n\n{text}\n\nДата публикации: {date}\nСсылка: https://vk.com/wall{owner_id}_{id_post}"
             time.sleep(3)
-            
+
             task = loop.create_task(send_message(result))
             loop.run_until_complete(task)
 
@@ -94,8 +138,4 @@ def from_posts_get_post(data):
             print('Пост не за сегодня')
             pass
 
-
-
-
-
-#user_link_parse(url)
+user_link_parse(url)
